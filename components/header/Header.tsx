3c3dfd6c8db2ab0e2b6cf538765d5ac405d5d99c
Viktor Gunin <guninvk@gmail.com>
2021-02-14 22:25:48 +0300

    init

---
+++ components/header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import styles from "./Header.module.css";
+export const Header: React.FC = () => (
+    <header className={styles.header}>
+        <h1>Memento</h1>
+    </header>
+);
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Header } from "../components/header/Header";
+
+const HomePage: React.FC = () => (
+    <main>
+        <Header />
+    </main>
+);
+export default HomePage;
